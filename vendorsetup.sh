ebda45a30ed235d8f9636537515d666b6d2013df
XxViRUsPRO <xxvirus.15@hotmail.com>
2024-04-29 12:17:26 +0300

    move ofrp stuff to vendorsetup.sh

---
+++ vendorsetup.sh
@@ -0,0 +1,10 @@
+export OF_MAINTAINER="xxvirus15"
+
+
+export FOX_BUILD_DEVICE=c2q
+export OF_DISABLE_MIUI_SPECIFIC_FEATURES=1
+export FOX_VANILLA_BUILD=1
+export OF_FORCE_PREBUILT_KERNEL=1
+export OF_FLASHLIGHT_ENABLE=1
+export OF_FL_PATH1="/sys/devices/virtual/camera/flash/rear_flash"
+export OF_FORCE_USE_RECOVERY_FSTAB=1
